3309c9a68c9512a496b578e8bf913fa723820e44
Sofía Mercado Moreno <sofiamercadomoreno@Sofias-MacBook-Pro.local>
2026-05-12 14:50:19 -0600

    Dashboard

--- moonbloom/src/app/features/dashboard/dashboard-home/dashboard-home.ts
+++ moonbloom/src/app/features/dashboard/dashboard-home/dashboard-home.ts
@@ -1,9 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard-home.html',
-  styleUrl: './dashboard-home.scss',
+  styleUrl: './dashboard-home.scss'
 })
-export class DashboardHomeComponent {}
+export class DashboardHomeComponent {
+
+  // TEMPORAL
+  // después aquí conectaremos el backend
+
+  user = {
+    name: ''
+  };
+
+  totalCycles = 0;
+
+  totalLogs = 0;
+
+  lastLog: any = null;
+
+  lastCycle: any = null;
+
+}
